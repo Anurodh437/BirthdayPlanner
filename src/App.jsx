4d075a83060e43e4f28bba555a656e720a5fb298
Anurodh437 <anurodhdubey437@gmail.com>
2021-11-24 20:19:09 +0530

    changed routing and added validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import "./App.css";
-import Login from "./Pages/Auth/SignUp/SignUp";
-import SignUp from "./Pages/Auth/Login/Login"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Authpage from "./Pages/Auth/AuthPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-        </Routes>
+        <Switch>
+          <Route path={["/login", "/signup"]} exact>
+            {<Authpage />}
+          </Route>
+        </Switch>
       </Router>
-      {/* <SignUp/> */}
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/Pages/Auth/AuthPage.jsx
+++ src/Pages/Auth/AuthPage.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "../Auth/Login/Login";
 import SignUp from "../Auth/SignUp/SignUp";
 
 const Authpage = () => {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route exact path="/login">
           <Login />
         </Route>
         <Route exact path="/signup">
           <SignUp />
         </Route>
-      </Routes>
+      </Switch>
     </Router>
   );
 };
