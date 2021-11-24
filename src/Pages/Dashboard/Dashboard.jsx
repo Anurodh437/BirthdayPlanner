d69f906f70cc8477eb6c45c34caf70ed20710110
Utkarsh <utkarshrkt2001@gmail.com>
2021-11-24 19:39:59 +0530

    ✅ feat : Designed Dashboard Page

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Dashboard.css";
+import mainImage from "../../assets/image_processing20211110-13169-mniu50.png";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <header>
+        <h1 className="logo">BirthdayPlanner</h1>
+        <div className="links">
+          <ul>
+            <li>About Us</li>
+            <li>Testimonials</li>
+            <li>Services</li>
+            <li>Contact</li>
+            <img
+              src="https://image.flaticon.com/icons/png/512/2922/2922510.png"
+              alt=""
+              className="profile"
+            />
+          </ul>
+        </div>
+      </header>
+      <section className="banner">
+        <div className="left">
+          <h1>
+            Birthday Planning <br /> done right.
+          </h1>
+          <p className="subtitle">
+            Plan best online birthdays <br /> for your loved ones for free .
+          </p>
+          <div className="buy">Plan it Now</div>
+        </div>
+        <img className="banner_img" src={mainImage} alt="" />
+
+        <div className="right">
+          <div className="quality">
+            <div className="icon">
+              <i className="fas fa-tint"></i>
+            </div>
+            <div className="data">
+              <h1>Personalized birthday messages</h1>
+              <p>Lorem</p>
+            </div>
+          </div>
+          <div className="quality">
+            <div className="icon">
+              <i className="fas fa-weight"></i>
+            </div>
+            <div className="data">
+              <h1>Gift Recomendations</h1>
+              <p>Best Gift recomendation for your birthday person.</p>
+            </div>
+          </div>
+          <div className="quality">
+            <div className="icon">
+              <i className="fas fa-ruler-vertical"></i>
+            </div>
+            <div className="data">
+              <h1>Nereby Restaurants</h1>
+              <p>8.25 x 31.875</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Dashboard;
