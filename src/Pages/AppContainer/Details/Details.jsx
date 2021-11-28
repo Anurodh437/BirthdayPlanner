c86dbe47379bb572431e6fee515088f26398cfc8
Anurodh437 <anurodhdubey437@gmail.com>
2021-11-29 01:10:29 +0530

    made changes in UI of gifts page

--- src/Pages/AppContainer/Details/Details.jsx
+++ src/Pages/AppContainer/Details/Details.jsx
@@ -47,27 +47,41 @@ const Details = () => {
 
       {/* Notes Card Container */}
       <div className="card_container">
-        <a class="gift_card1" href="/gifts">
-          <h3 className="card_head">Gifts</h3>
-          <p class="small">
-            Find the best gift for your loved ones on their birthday Lorem ipsum
-            dolor sit amet consectetur adipisicing
-          </p>
-          <div class="go-corner" href="#">
-            <div class="go-arrow">→</div>
-          </div>
-        </a>
+        <div className="w-96 linear-img">
+          <img
+            src="https://images.unsplash.com/photo-1530103862676-de8c9debad1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
+        </div>
+        <div className="flex mt-7">
+          <a class="gift_card1" href="/gifts">
+            <h3 className="card_head">Gifts</h3>
+            <p class="small">
+              Find the best gift for your loved ones on their birthday Lorem
+              ipsum dolor sit amet consectetur adipisicing
+            </p>
+            <div class="go-corner" href="#">
+              <div class="go-arrow">→</div>
+            </div>
+          </a>
 
-        <a class="gift_card2" href="/restaurants">
-          <h3 className="card_head">Restaurants & Hotels</h3>
-          <p class="small">
-            Find some of the best restaurants near your place to chill out with
-            your family and friends on birthdayys.
-          </p>
-          <div class="go-corner2" href="#">
-            <div class="go-arrow2">→</div>
-          </div>
-        </a>
+          <a class="gift_card2" href="/restaurants">
+            <h3 className="card_head">Restaurants & Hotels</h3>
+            <p class="small">
+              Find some of the best restaurants near your place to chill out
+              with your family and friends on birthdayys.
+            </p>
+            <div class="go-corner2" href="#">
+              <div class="go-arrow2">→</div>
+            </div>
+          </a>
+        </div>
+        <div className="w-96 linear-img" >
+          <img
+            src="https://images.unsplash.com/photo-1530103862676-de8c9debad1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
